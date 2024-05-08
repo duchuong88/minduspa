8e767874c2121b974642f2e909538bc00a273d18
tuanteng96 <30353855+tuanteng96@users.noreply.github.com>
2024-05-08 11:36:59 +0700

    first commit

---
+++ cser21_ios/Pods/Target Support Files/Pods-minduspa-minduspaUITests/Pods-minduspa-minduspaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_minduspa_minduspaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_minduspa_minduspaUITests
+@end
---
+++ cser21_ios/Pods/Target Support Files/Pods-minduspaTests/Pods-minduspaTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_minduspaTests : NSObject
+@end
+@implementation PodsDummy_Pods_minduspaTests
+@end
